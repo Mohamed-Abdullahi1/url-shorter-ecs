a34b5ebd072ab52f7f44f177994f56be583445e6
Mohamed-Abdullahi1 <Mohamedabdullahisgf@gmail.com>
2026-06-29 12:01:40 +0100

    feat: wire vpc and ecr modules into dev environment

---
+++ infra/envs/dev/main.tf
@@ -0,0 +1,17 @@
+module "vpc" {
+  source = "../../modules/vpc"
+
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  availability_zones   = var.availability_zones
+}
+
+module "ecr" {
+  source = "../../modules/ecr"
+
+  project_name     = var.project_name
+  repository_names = var.repository_names
+
+  tags = var.tags
+}
---
+++ infra/envs/dev/variables.tf
@@ -0,0 +1,39 @@
+variable "project_name" {
+  type    = string
+  default = "url-shortener"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "availability_zones" {
+  type    = list(string)
+  default = ["eu-west-2a", "eu-west-2b"]
+}
+
+variable "repository_names" {
+  type    = list(string)
+  default = ["api"]
+}
+
+variable "tags" {
+  type = map(string)
+
+  default = {
+    Project     = "url-shortener"
+    Environment = "dev"
+    ManagedBy   = "Terraform"
+  }
+}
