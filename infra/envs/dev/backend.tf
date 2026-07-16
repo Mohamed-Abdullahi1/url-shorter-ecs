33ccc82d3f02e16c2d89cb94d7c6f3ec218f0625
Mohamed-Abdullahi1 <Mohamedabdullahisgf@gmail.com>
2026-07-16 12:11:02 +0100

    fix: correct Terraform backend state key

--- infra/envs/dev/backend.tf
+++ infra/envs/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "url-tf-state-454374565233"
-    key          = "infra/dev/terrafrom.tfstate"
+    key          = "infra/dev/terraform.tfstate"
     region       = "eu-west-2"
     encrypt      = true
     use_lockfile = true
